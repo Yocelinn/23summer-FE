d179e76f4652256aeeabe723a7fc8a69199752f7
TerryRy <2505293361@qq.com>
2023-08-24 21:31:08 +0800

    day1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
-import Dropdown from 'vue-devui/dropdown';
-import 'vue-devui/dropdown/style.css';
-import Button from 'vue-devui/button';
-import 'vue-devui/button/style.css'
-import Splitter from 'vue-devui/splitter';
-import 'vue-devui/splitter/style.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import Table from 'vue-devui/table';
-import 'vue-devui/table/style.css';
 import DevUI from 'vue-devui';
 import 'vue-devui/style.css';
 import '@devui-design/icons/icomoon/devui-icon.css';
@@ -23,10 +15,6 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
-app.use(Dropdown)
-app.use(Button)
-app.use(Splitter)
 app.use(ElementPlus)
 app.use(DevUI)
-app.use(Table)
 app.mount('#app')
