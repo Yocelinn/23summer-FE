e0283b8d236c0b867a6e0a55d617def08bb8fce2
Brandy-L <1596001048@qq.com>
2023-08-24 21:42:02 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
-const app = createApp(App)
+import store from './store'
 
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+import 'element-plus/dist/index.css'
+
+import * as icons from '@element-plus/icons'
+
+const app=createApp(App)
+Object.keys(icons).forEach(key => {
+    app.component(key, icons[key])
+})
+
+
+
+app.use(store).use(router).use(ElementPlus).mount('#app')
+
+
